eabeb91bea11931adeeb88f2750f4e8c66ebca57
robert <robert.mn92@gmail.com>
2025-01-20 23:32:50 +0100

    add handleClick

--- src/components/Promo.jsx
+++ src/components/Promo.jsx
@@ -1,12 +1,20 @@
 function Promo(){
+
+    const handleClick = () => {
+        console.log("You clicked the button!")
+    }
     return(
         <div className="promo-section">
-        <div>
-            <h1>Don't miss this deal!</h1>
-            </div>
             <div>
-            <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
-        </div>
+                <h1>Don't miss this deal!</h1>
+                </div>
+                <div>
+                <h2>Subscribe to my newsletter and get all the shop items at 50% off!</h2>
+            </div>
+
+            <button onClick={handleClick}>
+                Click here
+            </button>
     </div>
     )
 }
