2b7cfffe7f20ee9b80a4e515b0a3118305639ca2
robert <robert.mn92@gmail.com>
2025-01-27 23:34:51 +0100

    add more inputs to form

--- src/components/FormBasic.jsx
+++ src/components/FormBasic.jsx
@@ -1,19 +1,36 @@
 function FormBasic(){
     const [name, setName] = useState('');
+    const [score, setScore] = useState('');
+    const [comment, setComment] = useState('');
 
     const handleSubmit = (e) => {
         e.preventDefault();
         setName(""); // clean input name
+        if(Number(score) <= 5 && comment.length < 10){
+            alert(`Please provide a comment with at least 10 characters for a score of 5 or less`);
+            return
+        }
         console.log("form submited")
-        alert(`Name: ${name}`);
-    }
+        setComment("")
+        setScore("10")
+    }   
     return(
-            <form onSubmit={handleSubmit}>
+            <form onsetScoreubmit={handleSubmit}>
                 <fieldset>
                     <div className="field">   
                         <label htmlFor="name">Name:</label>
                         <input id="name" type="text" placeholder="Name" name="name" value={name} 
                         onChange={e => setName(e.target.value)}/>
+                    </div>
+                    <div className="field">   
+                        <label htmlFor="score">Score: {score}</label>
+                        <input id="score" type="range" min="0" max="10" name="score" value={score} 
+                        onChange={e => setScore(e.target.value)}/>
+                    </div>
+                    <div className="field">   
+                        <label htmlFor="comment">Comment: {score}</label>
+                        <textarea id="comment"   name="comment" value={comment} 
+                        onChange={e => setComment(e.target.value)}/>
                     </div>
                     <button type="submit" disabled={!name}  >Submit</button>
                 </fieldset>
