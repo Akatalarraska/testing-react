479111bbcb69f5291f893308ca3cd391cfb8c77d
robert <robert.mn92@gmail.com>
2025-02-02 16:54:30 +0100

    add radioforms

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -3,8 +3,12 @@ import Intro2 from './Intro2.jsx';
 import Intro3 from './Intro3.jsx';  
 import Promo from './Promo.jsx';
 import FormBasic from './FormBasic.jsx';
+import { RadioGroup, RadioOption } from "./RadioForms.jsx";
+import { useState } from "react";
 
 function HomePage(){
+    const [selected, setSelected] = useState("");
+
     return(
         <div>
             <FormBasic />
@@ -12,6 +16,14 @@ function HomePage(){
             <Intro2 />
             <Intro3 />
             <Promo />
+            <h2>How did you hear about Little Lemon?</h2>
+            <RadioGroup onChange={setSelected} selected={selected}>
+                <RadioOption value="social_media">Social Media</RadioOption>
+                <RadioOption value="friends">Friends</RadioOption>
+                <RadioOption value="advertising">Advertising</RadioOption>
+                <RadioOption value="other">Other</RadioOption>
+            </RadioGroup>
+            <button disabled={!selected}>Submit</button>
         </div>
     )
 }
